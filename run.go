08359c0e19aa622567385104880162eeb383b3d9
David Tai <david@verus.io>
2016-06-29 17:16:30 -0500

    works now

---
+++ run.go
@@ -0,0 +1,332 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/flosch/pongo2"
+	"github.com/gocarina/gocsv"
+	"github.com/hanzo-io/oauthful"
+	"github.com/skratchdot/open-golang/open"
+)
+
+var redirectHTML = pongo2.Must(pongo2.FromFile("templates/redirect.html"))
+
+type FacebookFlow struct {
+}
+
+func (f FacebookFlow) Decode(req *http.Request) (*oauthful.AuthorizationResponse, error) {
+	errStr := req.FormValue("error")
+	if errStr != "" {
+		return nil, errors.New(errStr + "\n" + req.FormValue("error_reason") + "\n" + req.FormValue("error_description"))
+	}
+
+	res := &oauthful.AuthorizationResponse{
+		State: req.FormValue("state"),
+		Code:  req.FormValue("code"),
+	}
+
+	return res, nil
+}
+
+func (f FacebookFlow) Verify(res *oauthful.AuthorizationResponse) error {
+	if res.State != Config.State {
+		return errors.New("Authorization Error, State Does Not Match")
+	}
+	return nil
+}
+
+func (f FacebookFlow) AddParams(vals *url.Values) error {
+	vals.Add("client_id", Config.ClientId)
+	vals.Add("client_secret", Config.ClientSecret)
+	vals.Add("redirect_uri", Config.RedirectUri)
+	return nil
+}
+
+func FileExists(dir string) bool {
+	if _, err := os.Stat(dir); err == nil {
+		return true
+	}
+	return false
+}
+
+func MkDir(dir string) {
+	if !FileExists(dir) {
+		os.MkdirAll(dir, os.ModePerm)
+	}
+}
+
+func main() {
+	toks, ok := getOAuthTokens()
+	if !ok {
+		fmt.Println("Getting new tokens.")
+		toks, ok = newOAuthTokens()
+		if !ok {
+			fmt.Println("Could not get new tokens.")
+			return
+		}
+	}
+
+	csvExport(toks)
+}
+
+// Export all CSVs since last date (and update last one just in case)
+func csvExport(toks *oauthful.AccessTokenResponse) {
+	date := Date{Config.FirstDate}
+
+	f, err := os.Open(Config.DataPath + "/date")
+	if err == nil {
+		err = Decode(f, &date)
+		if err != nil {
+			date.Date = Config.FirstDate
+		}
+	}
+
+	MkDir(Config.ExportPath)
+	now := time.Now()
+
+	for false && now.After(date.Date) {
+		fmt.Printf("Querying from Date %v\n", date.Date)
+
+		ioutil.WriteFile(Config.DataPath+"/date", EncodeBytes(date), os.ModePerm)
+
+		if err := writeFile(toks, Config.QueryByAudience, "audience", date.Date); err != nil {
+			return
+		}
+
+		if err := writeFile(toks, Config.QueryByDevice, "device", date.Date); err != nil {
+			return
+		}
+
+		if err := writeFile(toks, Config.QueryByCountry, "country", date.Date); err != nil {
+			return
+		}
+
+		date.Date = date.Date.Add(time.Hour * 24)
+	}
+
+	mergeFiles("audience")
+	mergeFiles("device")
+	mergeFiles("country")
+}
+
+// Merge the CSV for a specific dataset
+func mergeFiles(prefix string) error {
+	exportPath := Config.ExportPath
+
+	fileInfos, err := ioutil.ReadDir(exportPath + "/" + prefix)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Merging %v.csv\n", prefix)
+
+	filename := exportPath + "/" + prefix + ".csv"
+
+	_, _ = os.OpenFile(filename, os.O_CREATE, os.ModePerm)
+	fh, err := os.OpenFile(filename, os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	w := bufio.NewWriter(fh)
+
+	first := true
+	for _, fileInfo := range fileInfos {
+		fn := fileInfo.Name()
+
+		f, _ := os.Open(exportPath + "/" + prefix + "/" + fn)
+		defer f.Close()
+
+		// fmt.Printf("Merge %v\n", fn)
+
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanLines)
+
+		if !first {
+			scanner.Scan()
+		} else {
+			first = false
+		}
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			// fmt.Printf("Line %v\n", line)
+			fmt.Fprintln(w, line)
+		}
+	}
+	w.Flush() // Don't forget to flush!
+
+	return nil
+}
+
+// Write the CSV for a specific query for a specific date
+func writeFile(toks *oauthful.AccessTokenResponse, q, prefix string, date time.Time) error {
+	MkDir(Config.ExportPath + "/" + prefix)
+
+	filename := Config.ExportPath + "/" + prefix + "/" + prefix + date.Format("_2006-01-02") + ".csv"
+	// if prefix == "audience" || prefix == "device" {
+	// 	if FileExists(filename) {
+	// 		return nil
+	// 	}
+	// }
+
+	insights := make([]FacebookInsight, 0)
+
+	fmt.Printf("Using Query for %v\n", prefix)
+
+	err := queryForDate(q, toks, date, &insights)
+	if err != nil {
+		fmt.Printf("Query Error for %v: %v\n", prefix, err)
+		return err
+	}
+
+	csv, err := gocsv.MarshalString(insights)
+	if err != nil {
+		fmt.Printf("CSV Error for %v: %v\n", prefix, err)
+		return err
+	}
+	ioutil.WriteFile(filename, []byte(csv), os.ModePerm)
+
+	return nil
+}
+
+// Query Facebook Graph API for a specific date
+func queryForDate(q string, toks *oauthful.AccessTokenResponse, date time.Time, insights *[]FacebookInsight) error {
+	since := date.Add(time.Hour * -24)
+	until := date
+
+	insightsPart := make([]FacebookInsight, 0)
+
+	url := fmt.Sprintf(q, Config.AppId, toks.AccessToken, since.Format("2006-01-02"), until.Format("2006-01-02"))
+
+	wrapper := FacebookInsightWrapper{Paging: FacebookPaging{Next: Config.GraphUrl + url}}
+
+	for wrapper.Paging.Next != "" {
+		nextUrl := wrapper.Paging.Next
+		// fmt.Printf("Url: %v\n", nextUrl)
+
+		res, err := http.Get(nextUrl)
+		if err != nil {
+			return err
+		}
+
+		wrapper = FacebookInsightWrapper{}
+		wrapper.Data = &insightsPart
+
+		err = Decode(res.Body, &wrapper)
+		if err != nil {
+			return err
+		}
+
+		// Retry if out of api, otherwise its super annoying
+		if wrapper.Error.Message != "" {
+			if strings.Contains(wrapper.Error.Message, "#17") {
+				wrapper.Paging.Next = nextUrl
+				fmt.Println("Out of API, waiting 10 minutes before retrying...")
+				time.Sleep(time.Second * 600)
+				continue
+			}
+			return errors.New(wrapper.Error.Message)
+		}
+
+		for _, insight := range insightsPart {
+			*insights = append((*insights), insight)
+		}
+
+		if wrapper.Paging.Next != "" {
+			fmt.Println("Loading Next Page")
+		} else {
+			fmt.Println("All Pages Loaded")
+		}
+	}
+
+	for i, insight := range *insights {
+		(*insights)[i].ActionsStr = flattenJson(Encode(insight.Actions))
+		(*insights)[i].WebsiteCtrStr = flattenJson(Encode(insight.WebsiteCtr))
+	}
+
+	return nil
+}
+
+// Flatten the JSON so it doesn't screw up the Good Data Parser
+func flattenJson(str string) string {
+	return strings.Replace(strings.Replace(strings.Replace(str, "\"", "", -1), " ", "", -1), "\n", "", -1)
+}
+
+// Load OAuth Tokens from File if possible
+func getOAuthTokens() (*oauthful.AccessTokenResponse, bool) {
+	f, err := os.Open(Config.TokensPath)
+	if err != nil {
+		return nil, false
+	}
+
+	fmt.Println("Loading Tokens")
+	toks := &oauthful.AccessTokenResponse{}
+
+	err = Decode(f, toks)
+	if err != nil {
+		return nil, false
+	}
+
+	fmt.Println("Decoding Tokens")
+	return toks, true
+}
+
+// Issue an OAuth2.0 Authorization Request
+func newOAuthTokens() (*oauthful.AccessTokenResponse, bool) {
+	go server()
+
+	qs := []string{
+		"client_id=" + Config.ClientId,
+		"state=" + Config.State,
+		"scope=" + Config.Scope,
+		"redirect_uri=" + Config.RedirectUri,
+	}
+
+	url := Config.AuthorizeUrl + "?" + strings.Join(qs, "&")
+
+	open.Run(url)
+
+	time.Sleep(time.Second * 2)
+
+	return getOAuthTokens()
+}
+
+// OAuth2.0 Redirect Handler
+func oauthRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	hc := &http.Client{}
+	client := oauthful.New(hc, Config.TokenUrl, FacebookFlow{})
+
+	pctx := pongo2.Context{
+		"success": true,
+	}
+
+	if res, err := client.Handle(r); err != nil {
+		pctx["success"] = false
+		pctx["error"] = err.Error()
+	} else {
+		pctx["response"] = res
+	}
+
+	err := redirectHTML.ExecuteWriter(pctx, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	MkDir(Config.DataPath)
+	ioutil.WriteFile(Config.TokensPath, EncodeBytes(pctx["response"]), os.ModePerm)
+}
+
+// Run the server that waits to execute the OAuth2.0 Redirect Handler
+func server() {
+	http.HandleFunc("/redirect", oauthRedirectHandler)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ types.go
@@ -0,0 +1,135 @@
+package main
+
+import "time"
+
+type AdsActionStats struct {
+	// Click1d  float64 `json:"1d_click"`
+	// View1d   float64 `json:"1d_view"`
+	// Click28d float64 `json:"28d_click"`
+	// View28d  float64 `json:"28d_view"`
+	// Click7d  float64 `json:"7d_click"`
+	// View7d   float64 `json:"7d_view"`
+
+	// ActionCarouselCardId   string  `json:"action_carousel_card_id"`
+	// ActionCarouselCardName string  `json:"action_carousel_card_name"`
+	// ActionDestination      string  `json:"action_destination"`
+	// ActionDevice           string  `json:"action_device"`
+	// ActionTargetId         string  `json:"action_target_id"`
+	ActionType string `json:"action_type"`
+	// ActionVideoType        string  `json:"action_video_type"`
+	Value float64 `json:"value"`
+}
+
+type Date struct {
+	Date time.Time `json:"date"`
+}
+
+type FacebookError struct {
+	Message     string `json:"message"`
+	Type        string `json:"type"`
+	Code        int64  `json:"code"`
+	IsTransient bool   `json:"is_transient"`
+	FBTraceId   string `json:"fbtrace_id"`
+}
+
+type FacebookPaging struct {
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+}
+
+type FacebookInsightWrapper struct {
+	Data   *[]FacebookInsight `json:"data"`
+	Error  FacebookError      `json:"error"`
+	Paging FacebookPaging     `json:"paging"`
+}
+
+type FacebookInsight struct {
+	AccountId                                 string `json:"account_id,omitempty" csv:"account_id"`
+	AccountName                               string `json:"account_name,omitempty" csv:"account_name"`
+	AdId                                      string `json:"ad_id,omitempty" csv:"ad_id"`
+	AdName                                    string `json:"ad_name,omitempty" csv:"ad_name"`
+	AdsetId                                   string `json:"adset_id,omitempty" csv:"adset_id"`
+	AdsetName                                 string `json:"adset_name,omitempty" csv:"adset_name"`
+	Age                                       string `json:"age,omitempty" csv:"age"`
+	BuyingType                                string `json:"buying_type,omitempty" csv:"buying_type"`
+	CampaignId                                string `json:"campaign_id,omitempty" csv:"campaign_id"`
+	CampaignName                              string `json:"campaign_name,omitempty" csv:"campaign_name"`
+	Country                                   string `json:"country,omitempty" csv:"country"`
+	FrequencyValue                            string `json:"frequency_value,omitempty" csv:"frequency_value"`
+	Gender                                    string `json:"gender,omitempty" csv:"gender"`
+	HourlyStatsAggregatedByAdvertiserTimeZone string `json:"hourly_stats_aggregated_by_advertiser_time_zone,omitempty" csv:"hourly_stats_aggregated_by_advertiser_time_zone"`
+	HourlyStatsAggregatedByAudienceTimeZone   string `json:"hourly_stats_aggregated_by_audience_time_zone,omitempty" csv:"hourly_stats_aggregated_by_audience_time_zone"`
+	ImpressionDevice                          string `json:"impression_device,omitempty" csv:"impression_device"`
+	Impressions                               string `json:"impressions,omitempty" csv:"impressions"`
+	Objective                                 string `json:"objective,omitempty" csv:"objective"`
+	PlacePageId                               string `json:"place_page_id,omitempty" csv:"place_page_id"`
+	PlacePageName                             string `json:"place_page_name,omitempty" csv:"place_page_name"`
+	Placement                                 string `json:"placement,omitempty" csv:"placement"`
+	ProductId                                 string `json:"product_id,omitempty" csv:"product_id"`
+	Region                                    string `json:"region,omitempty" csv:"region"`
+
+	DateStart string `json:"date_start,omitempty" csv:"date_start"`
+	DateStop  string `json:"date_stop,omitempty" csv:"date_stop"`
+
+	AppStoreClicks               int64   `json:"app_store_clicks,omitempty" csv:"app_store_clicks"`
+	CallToActionClicks           int64   `json:"call_to_action_clicks,omitempty" csv:"call_to_action_clicks"`
+	CanvasAvgViewPercent         float64 `json:"canvas_avg_view_percent,omitempty" csv:"canvas_avg_view_percent"`
+	CanvasAvgViewTime            float64 `json:"canvas_avg_view_time,omitempty" csv:"canvas_avg_view_time"`
+	Clicks                       int64   `json:"clicks,omitempty" csv:"clicks"`
+	CostPerInlineLinkClick       float64 `json:"cost_per_inline_link_click,omitempty" csv:"cost_per_inline_link_click"`
+	CostPerInlinePostEngagement  float64 `json:"cost_per_inline_post_engagement,omitempty" csv:"cost_per_inline_post_engagement"`
+	CostPerTotalAction           float64 `json:"cost_per_total_action,omitempty" csv:"cost_per_total_action"`
+	CostPerUniqueClick           float64 `json:"cost_per_unique_click,omitempty" csv:"cost_per_unique_click"`
+	CostPerUniqueInlineLinkClick float64 `json:"cost_per_unique_inline_link_click,omitempty" csv:"cost_per_unique_inline_link_click"`
+	Cpc                          float64 `json:"cpc,omitempty" csv:"cpc"`
+	Cpm                          float64 `json:"cpm,omitempty" csv:"cpm"`
+	Cpp                          float64 `json:"cpp,omitempty" csv:"cpp"`
+	Ctr                          float64 `json:"ctr,omitempty" csv:"ctr"`
+	DeeplinkClicks               int64   `json:"deeplink_clicks,omitempty" csv:"deeplink_clicks"`
+	Frequency                    float64 `json:"frequency,omitempty" csv:"frequency"`
+	InlineLinkClickCtr           float64 `json:"inline_link_click_ctr,omitempty" csv:"inline_link_click_ctr"`
+	InlineLinkClicks             int64   `json:"inline_link_clicks,omitempty" csv:"inline_link_clicks"`
+	InlinePostEngagement         int64   `json:"inline_post_engagement,omitempty" csv:"inline_post_engagement"`
+	NewsfeedAvgPosition          float64 `json:"newsfeed_avg_position,omitempty" csv:"newsfeed_avg_position"`
+	NewsfeedClicks               int64   `json:"newsfeed_clicks,omitempty" csv:"newsfeed_clicks"`
+	NewsfeedImpressions          int64   `json:"newsfeed_impressions,omitempty" csv:"newsfeed_impressions"`
+	Reach                        int64   `json:"reach,omitempty" csv:"reach"`
+	SocialClicks                 int64   `json:"social_clicks,omitempty" csv:"social_clicks"`
+	SocialImpressions            int64   `json:"social_impressions,omitempty" csv:"social_impressions"`
+	SocialReach                  int64   `json:"social_reach,omitempty" csv:"social_reach"`
+	SocialSpend                  float64 `json:"social_spend,omitempty" csv:"social_spend"`
+	Spend                        float64 `json:"spend,omitempty" csv:"spend"`
+	TotalActionValue             float64 `json:"total_action_value,omitempty" csv:"total_action_value"`
+	TotalActions                 int64   `json:"total_actions,omitempty" csv:"total_actions"`
+	TotalUniqueActions           int64   `json:"total_unique_actions,omitempty" csv:"total_unique_actions"`
+	UniqueClicks                 int64   `json:"unique_clicks,omitempty" csv:"unique_clicks"`
+	UniqueCtr                    float64 `json:"unique_ctr,omitempty" csv:"unique_ctr"`
+	UniqueImpressions            int64   `json:"unique_impressions,omitempty" csv:"unique_impressions"`
+	UniqueInlineLinkClickCtr     float64 `json:"unique_inline_link_click_ctr,omitempty" csv:"unique_inline_link_click_ctr"`
+	UniqueInlineLinkClicks       int64   `json:"unique_inline_link_clicks,omitempty" csv:"unique_inline_link_clicks"`
+	UniqueLinkClicksCtr          float64 `json:"unique_link_clicks_ctr,omitempty" csv:"unique_link_clicks_ctr"`
+	UniqueSocialClicks           int64   `json:"unique_social_clicks,omitempty" csv:"unique_social_clicks"`
+	UniqueSocialImpressions      int64   `json:"unique_social_impressions,omitempty" csv:"unique_social_impressions"`
+	WebsiteClicks                int64   `json:"website_clicks,omitempty" csv:"website_clicks"`
+
+	// ActionValues                []AdsActionStats `json:"action_values,omitempty" csv:"action_values"`
+	Actions    []AdsActionStats `json:"actions,omitempty" csv:"-"`
+	ActionsStr string           `csv:"actions"`
+	// CostPer10SecVideoView       []AdsActionStats `json:"cost_per_10_sec_video_view,omitempty" csv:"cost_per_10_sec_video_view"`
+	// CostPerActionType           []AdsActionStats `json:"cost_per_action_type,omitempty" csv:"cost_per_action_type"`
+	// CostPerUniqueActionType     []AdsActionStats `json:"cost_per_unique_action_type,omitempty" csv:"cost_per_unique_action_type"`
+	// UniqueActions               []AdsActionStats `json:"unique_actions,omitempty" csv:"unique_actions"`
+	// Video10SecWatchedActions    []AdsActionStats `json:"video_10_sec_watched_actions,omitempty" csv:"video_10_sec_watched_actions"`
+	// Video15SecWatchedActions    []AdsActionStats `json:"video_15_sec_watched_actions,omitempty" csv:"video_15_sec_watched_actions"`
+	// Video30SecWatchedActions    []AdsActionStats `json:"video_30_sec_watched_actions,omitempty" csv:"video_30_sec_watched_actions"`
+	// VideoAvgPctWatchedActions   []AdsActionStats `json:"video_avg_pct_watched_actions,omitempty" csv:"video_avg_pct_watched_actions"`
+	// VideoAvgSecWatchedActions   []AdsActionStats `json:"video_avg_sec_watched_actions,omitempty" csv:"video_avg_sec_watched_actions"`
+	// VideoCompleteWatchedActions []AdsActionStats `json:"video_complete_watched_actions,omitempty" csv:"video_complete_watched_actions"`
+	// VideoP100WatchedActions     []AdsActionStats `json:"video_p100_watched_actions,omitempty" csv:"video_p100_watched_actions"`
+	// VideoP25WatchedActions      []AdsActionStats `json:"video_p25_watched_actions,omitempty" csv:"video_p25_watched_actions"`
+	// VideoP50WatchedActions      []AdsActionStats `json:"video_p50_watched_actions,omitempty" csv:"video_p50_watched_actions"`
+	// VideoP75WatchedActions      []AdsActionStats `json:"video_p75_watched_actions,omitempty" csv:"video_p75_watched_actions"`
+	// VideoP95WatchedActions      []AdsActionStats `json:"video_p95_watched_actions,omitempty" csv:"video_p95_watched_actions"`
+	WebsiteCtr    []AdsActionStats `json:"website_ctr,omitempty" csv:"-"`
+	WebsiteCtrStr string           `csv:"website_ctr"`
+}
